e7a4d7a433f0cbd5886b5c75ad2b67b9fe598687
beatrizac03 <beatrizaguiarc14@gmail.com>
2025-06-11 18:02:24 -0300

    feat: criação das 3 paginas e estilos

---
+++ api.js
@@ -0,0 +1,19 @@
+let prod = false
+
+let api_url = prod ? 'https://api-cuida-sus.onrender.com' : 'http://localhost:8080'
+
+export function logarFuncionario() {
+
+}
+
+export function cadastrarFuncionario() {
+
+}
+
+export function vincularSalaAoMedico() {
+
+}
+
+export function chamarPacienteNaSala() {
+
+}
